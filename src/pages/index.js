ba71cb4a399b2b90e432e94918b8ea13f6d528fd
Joe <jparkkennaby@gmail.com>
2020-07-03 23:05:50 +0100

    make image component dynamic. pass in gatsy astronaught image in index file

--- src/pages/index.js
+++ src/pages/index.js
@@ -12,7 +12,7 @@ const IndexPage = () => (
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
+      <Image alt="Gatsby in Space" filename="gatsby-astronaut.png" />
     </div>
     <Link to="/page-2/">Go to page 2</Link> <br />
     <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
