43e6d44f6830af61f84b79a352f32014a2b32501
Joe <jparkkennaby@gmail.com>
2020-07-04 16:46:50 +0100

    setup hero slide and promo button components

---
+++ src/components/promo-button/promo-button.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import '~components/promo-button/promo-button.scss';
+
+// TODO needs refactoring
+
+const PromoButton = ({linkAttributes, children}) => {
+    return ( 
+        <div className="promo-button">
+            <a className="promo-button__link"
+            href={linkAttributes.url}
+            title={linkAttributes.title}
+            target=""
+            >{children}</a>
+        </div>
+     );
+}
+ 
+export default PromoButton;
