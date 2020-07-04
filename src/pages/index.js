38c9c7eb3c8b336e883145b916e6e866d4e07373
Joe <jparkkennaby@gmail.com>
2020-07-04 12:01:43 +0100

    update seo component import statement to use alias path

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,7 @@ import { Link } from "gatsby"
 
 import Layout from "~components/layout/layout";
 import Image from "~components/image/image";
-import SEO from "../components/seo"
+import SEO from "~components/seo";
 
 const IndexPage = () => (
   <Layout>
