876cb2473a79a3fc09da681a463f8d81454446d8
Joe <jparkkennaby@gmail.com>
2020-07-07 22:28:19 +0100

    use react-grid for footer styles. remove unused footer styles

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -1,38 +1,41 @@
 import React from 'react';
 import Icon from '~components/icon/icon';
 import Navigation from '~components/navigation/navigation';
+import { GridContainer, Row, Col } from '~components/common/grid/grid';
 
 import '~components/footer/footer.scss';
 
 const Footer = ({menuLinks}) => {
     return ( 
         <footer className="site-footer">
-            <div class="site-footer__section">
+            <section class="site-footer__section">
 
-                <div className="site-footer__item">
-                    <p className="site-footer__title">About Us</p>
-                    <div className="site-footer__about">
-                        <Icon />
-                        <p className="site-footer__about-us">Wildflower Belt was established in Dorset, UK, as a small independent family business and is built on a love of high quality leather accessories.</p>
-                    </div>
-                </div>
+                <GridContainer>
+                    <Row>
+                        <Col sm={12} lg={6}>
+                            <p className="site-footer__title">About Us</p>
+                            <div className="site-footer__item">
+                                <Icon />
+                                <p className="site-footer__about-us">Wildflower Belt was established in Dorset, UK, as a small independent family business and is built on a love of high quality leather accessories.</p>
+                            </div>
+                        </Col>
+                        <Col sm={12} lg={6}>
+                            <p className="site-footer__title">Site Map</p>
+                                <div className="site-footer__item">
+                                    <Navigation menuLinks={menuLinks}/>
+                            </div>
+                        </Col>
+                    </Row>
+                </GridContainer>
+            </section>
 
-                <div className="site-footer__item">
-                    <div className="site-footer__site-map">
-                        <p className="site-footer__title">Site Map</p>
-                        <div className="site-footer__navigation">
-                            <Navigation menuLinks={menuLinks}/>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="site-footer__section">
+            <section class="site-footer__section">
 
                 <span className="site-footer__copyright">
                         © Wildflower Belt {new Date().getFullYear()}
                 </span>
 
-            </div>
+            </section>
         </footer>
      );
 }
