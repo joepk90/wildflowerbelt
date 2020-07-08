919fc5c2a229ff8c90b9ebbe85de4899d8ca07ad
Joe <jparkkennaby@gmail.com>
2020-07-08 20:42:30 +0100

    update index.js file to use section adn container components

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,6 +5,7 @@ import indexData from "~content/index.json"
 import Layout from "~components/layout/layout";
 import Image from "~components/image/image";
 import Title from "~components/title/title";
+import Container from "~components/container/container";
 import Section from "~components/section/section";
 import Content from "~components/content/content";
 import HeroSlide from "~components/hero-slide/hero-slide";
@@ -18,40 +19,45 @@ const IndexPage = () => (
     <SEO title="Home" />
     
     <Section>
-      <HeroSlide/> {/* should this be added here? potentially it should be passed into main? */}
+      <Container options={{fullWidth: true}}>
+        <HeroSlide/> {/* should this be added here? potentially it should be passed into main? */}
+      </Container>
     </Section>
     
-    <Section>
-      <Title options={{align: 'center'}}>{indexData.title}</Title>
-      <Content content={indexData.description} />
+    <Section options={{padding: true, margin: true}}>
+      <Container>
+        <Title options={{align: 'center'}}>{indexData.title}</Title>
+        <Content content={indexData.description} />
+      </Container>
     </Section>
 
 
     <Section>
-      <GridContainer>
-        <Row>
+      <Container>
+        <GridContainer>
+          <Row>
 
-          <Col xs={12} sm={4}>
-            <Link to="/product-details/">
-              <Image alt="Snap On Feature" filename="snap-on-feature.jpg" />
-            </Link>
-          </Col>
+            <Col xs={12} sm={4}>
+              <Link to="/product-details/">
+                <Image alt="Snap On Feature" filename="snap-on-feature.jpg" />
+              </Link>
+            </Col>
 
-          <Col xs={12} sm={4}>
-            <Link to="/product-details/">
-              <Image alt="Embossed Pattern" filename="embossed-pattern.jpg" />
-            </Link>
-          </Col>
+            <Col xs={12} sm={4}>
+              <Link to="/product-details/">
+                <Image alt="Embossed Pattern" filename="embossed-pattern.jpg" />
+              </Link>
+            </Col>
+            
+            <Col xs={12} sm={4}>
+              <Link to="/product-details/">
+                <Image alt="Free Shipping" filename="free-shipping.jpg" />
+              </Link>
+            </Col>
           
-          <Col xs={12} sm={4}>
-            <Link to="/product-details/">
-              <Image alt="Free Shipping" filename="free-shipping.jpg" />
-            </Link>
-          </Col>
-        
-        </Row>
-      </GridContainer>
-
+          </Row>
+        </GridContainer>
+      </Container>
     </Section>
   
     <Link to="/page-2/">Go to page 2</Link> <br />
