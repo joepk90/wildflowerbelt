946a8ec7eacb7c754dee527f8f1cf348f0e1dae4
Joe <jparkkennaby@gmail.com>
2020-07-08 21:11:38 +0100

    update footer JSX and scss. use section component to manage layout spacing

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import Icon from '~components/icon/icon';
 import Navigation from '~components/navigation/navigation';
+import Container from '~components/container/container';
+import Section from '~components/section/section';
 import { GridContainer, Row, Col } from '~components/common/grid/grid';
 
 import '~components/footer/footer.scss';
@@ -8,34 +10,39 @@ import '~components/footer/footer.scss';
 const Footer = ({menuLinks}) => {
     return ( 
         <footer className="site-footer">
-            <section class="site-footer__section">
+            <Section options={{paddingTopLarge: true, marginBottom: true}}>
+                <Container>
 
-                <GridContainer>
-                    <Row>
-                        <Col sm={12} lg={6}>
-                            <p className="site-footer__title">About Us</p>
-                            <div className="site-footer__item">
-                                <Icon />
-                                <p className="site-footer__about-us">Wildflower Belt was established in Dorset, UK, as a small independent family business and is built on a love of high quality leather accessories.</p>
-                            </div>
-                        </Col>
-                        <Col sm={12} lg={6}>
-                            <p className="site-footer__title">Site Map</p>
+                    <GridContainer>
+                        <Row>
+                            <Col sm={12} lg={6}>
+                                <p className="site-footer__title">About Us</p>
                                 <div className="site-footer__item">
-                                    <Navigation menuLinks={menuLinks}/>
-                            </div>
-                        </Col>
-                    </Row>
-                </GridContainer>
-            </section>
+                                    <Icon />
+                                    <p className="site-footer__about-us">Wildflower Belt was established in Dorset, UK, as a small independent family business and is built on a love of high quality leather accessories.</p>
+                                </div>
+                            </Col>
+                            <Col sm={12} lg={6}>
+                                <p className="site-footer__title">Site Map</p>
+                                    <div className="site-footer__item">
+                                        <Navigation menuLinks={menuLinks}/>
+                                </div>
+                            </Col>
+                        </Row>
+                    </GridContainer>
 
-            <section class="site-footer__section">
+                </Container>
+            </Section>
 
-                <span className="site-footer__copyright">
-                        © Wildflower Belt {new Date().getFullYear()}
-                </span>
+            <Section options={{paddingBottom: true}}>
+                <Container>
 
-            </section>
+                    <span className="site-footer__copyright">
+                            © Wildflower Belt {new Date().getFullYear()}
+                    </span>
+
+                </Container>
+            </Section>
         </footer>
      );
 }
