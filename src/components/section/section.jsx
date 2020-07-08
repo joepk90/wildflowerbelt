beca4f466518d9bc4f57bb9864ab35588438aba7
Joe <jparkkennaby@gmail.com>
2020-07-08 20:41:52 +0100

    setup container component with optional modifier properties. update section component to use optional modifier properties. delete global container class + layout.scss include

--- src/components/section/section.jsx
+++ src/components/section/section.jsx
@@ -2,9 +2,51 @@ import React from 'react';
 
 import "~components/section/section.scss";
 
-const Section = ({children}) => {
+const classModifiers = function(options) {
+
+    let classList = [];
+
+    if (!options) return '';
+
+    if (options.padding && options.padding === true) {
+        classList.push('page-section--padding');
+    }
+
+    if (options.paddingTop && options.paddingTop === true) {
+        classList.push('page-section--padding-top');
+    }
+
+    if (options.paddingBottom && options.paddingBottom === true) {
+        classList.push('page-section--padding-bottom');
+    }
+
+    if (options.margin && options.margin === true) {
+        classList.push('page-section--margin');
+    }
+
+    if (options.marginTop && options.marginTop === true) {
+        classList.push('page-section--margin-top');
+    }
+
+    if (options.marginBottom && options.marginBottom === true) {
+        classList.push('page-section--margin-bottom');
+    }
+
+    
+    if (classList.length === 0) {
+        return
+    }
+
+    return ' ' + classList.join(' ')
+
+}
+
+const Section = ({children, options}) => {
+
+    const modifiers = classModifiers(options);
+
     return ( 
-        <section className="page-section container">{children}</section>
+        <section className={'page-section ' + modifiers}>{children}</section>
      );
 }
  
