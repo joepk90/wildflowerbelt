b7e0f67b19ad25d91fd0ec6f2161e65c49a6b022
Joe <jparkkennaby@gmail.com>
2020-07-10 23:38:43 +0100

    setup wildflowerbelt page

---
+++ src/pages/wildflowerbelt.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+import Layout from "~components/layout/layout";
+import SEO from "~components/seo";
+
+const WildflowerBelt = () => (
+    <Layout>
+        <SEO title="Wildflower Belt" />
+
+    </Layout>
+)
+
+export default WildflowerBelt
