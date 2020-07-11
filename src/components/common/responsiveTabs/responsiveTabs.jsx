6cfd37d1201b87b4c20ca6dcf2b433d24ece76b4
Joe <jparkkennaby@gmail.com>
2020-07-11 13:46:42 +0100

    start responsive tab component setup

--- src/components/common/responsiveTabs/responsiveTabs.jsx
+++ src/components/common/responsiveTabs/responsiveTabs.jsx
@@ -6,9 +6,38 @@ class ResponsiveTabs extends Component {
 
     state = {}
 
+    handleTabClick = () => {
+
+    }
+
+    renderTabs = () => {
+
+        return (
+            <div className="responsive-tabs__tabs">
+                {this.props.tabsData.map((tab, index) => {
+
+                    if (!tab.title || !tab.content) return '';
+
+                    return <div onClick={this.handleTabClick} key={index} className="responsive-tabs__tab-title">{tab.title}</div>
+                })}
+            </div>
+
+        );
+    }
+
     render() {
+
+        if (this.props.tabsData.length === 0) return;
+
         return (
             <div className="responsive-tabs">
+
+
+                {this.renderTabs()}
+
+
+                <div className="responsive-tabs__content"></div>
+
 
             </div>
         );
--- src/pages/wildflowerbelt.js
+++ src/pages/wildflowerbelt.js
@@ -6,6 +6,7 @@ import Container from "~components/container/container";
 import Section from "~components/section/section";
 import ResponsiveTabs from "~components/common/responsiveTabs/responsiveTabs";
 import ProductDetails from "~components/productDetails/productDetails";
+import Content from "~components/content/content";
 import SEO from "~components/seo";
 
 const WildflowerBelt = () => (
@@ -20,7 +21,20 @@ const WildflowerBelt = () => (
 
         <Section options={{ paddingLarge: true }}>
             <Container>
-                <ResponsiveTabs></ResponsiveTabs>
+                <ResponsiveTabs tabsData={[
+                    {
+                        "title": "Descrition",
+                        "content": <Content content={WildflowerbeltData.description} />,
+                    },
+                    {
+                        "title": "Additional Information",
+                        "content": "",
+                    },
+                    {
+                        "title": "Reviews",
+                        "content": "",
+                    },
+                ]}></ResponsiveTabs>
             </Container>
         </Section>
 
