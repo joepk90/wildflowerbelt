adeb772b476108453266d3c4db8a3193bc290e6c
Joe <jparkkennaby@gmail.com>
2020-07-12 14:30:32 +0100

    update wildflowerbelt.json description data. update p keys to paragraph keys. update the content component to allow h5 elements

--- src/components/content/content.jsx
+++ src/components/content/content.jsx
@@ -7,6 +7,8 @@ const renderContentData = (data, index) => {
     // TODO make paragraph component?
     if ('paragraph' in data) {
         return (<p className="content__paragraph" key={index}>{data.paragraph}</p>);
+    } else if ('h5' in data) {
+        return (<h5 className="content__title" key={index}>{data.h5}</h5>);
     }
 }
 
