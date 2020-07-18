6fcc035cf0fadcc4c63fa38015bac96d16cf2de3
Joe <jparkkennaby@gmail.com>
2020-07-18 13:12:29 +0100

    remove gatsyb page links on the index.js page

--- src/pages/index.js
+++ src/pages/index.js
@@ -85,8 +85,6 @@ const IndexPage = () => (
       </Container>
     </Section>
 
-    <Link to="/page-2/">Go to page 2</Link> <br />
-    <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
   </Layout>
 )
 
