26500273f9344c198e7ac776892065d0c1b4f992
Joe <jparkkennaby@gmail.com>
2020-07-19 23:06:56 +0100

    update site title, description and favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,7 +3,7 @@ const pageLinks = require("./src/utilities/page-links.js");
 
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
+    title: `Wildflower Belt`,
     menuLinks: [
       {
         name: 'Home',
@@ -22,8 +22,8 @@ module.exports = {
         link: pageLinks.about,
       },
     ],
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    description: `Crafted from rich tan leather, this accessory boasts a hand-brushed, leafy embossed pattern that adds flair to anything you wear.`,
+    author: `Wildflower Belt Ltd`,
   },
   plugins: [
     `gatsby-plugin-sass`,
@@ -46,7 +46,7 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/wildflowerbelt-icon-440x406.png`, // This path is relative to the root of the site.
       },
     },
     {
