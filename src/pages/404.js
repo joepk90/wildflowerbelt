66a5baf40e5ef39c6103f01f5f4ec986d1022ea7
Joe <jparkkennaby@gmail.com>
2020-07-19 22:48:01 +0100

    setup 404 page layout

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,13 +1,35 @@
+import { Link } from "gatsby"
 import React from "react"
 
+import { pageLinks } from "~utilities/utilities.js"
 import Layout from "~components/layout/layout";
+import Container from "~components/container/container";
+import { GridContainer, Row, Col } from '~components/common/grid/grid';
+import Title from "~components/title/title";
+import Section from "~components/section/section";
 import SEO from "~components/seo";
 
 const NotFoundPage = () => (
   <Layout>
+
     <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+
+    <Section options={{ marginBottom: true }}>
+      <Container>
+
+        <GridContainer>
+          <Row>
+
+            <Col md={12} lg={9}>
+              <Title>Page Not Found</Title>
+              <p>Oops, you've just found a page that doesn&#39;t exist... click <Link to={pageLinks.home}>here</Link> to go to the homepage.</p>
+            </Col>
+
+          </Row>
+        </GridContainer>
+
+      </Container>
+    </Section>
   </Layout>
 )
 
